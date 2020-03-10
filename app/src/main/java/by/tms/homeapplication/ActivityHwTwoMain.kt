c6c53ce836110d53910c32a260f1210c6d3091ce
Slava <59572751+GH-Slav@users.noreply.github.com>
2020-03-10 23:20:48 +0300

    9 commit

---
+++ app/src/main/java/by/tms/homeapplication/ActivityHwTwoMain.kt
@@ -0,0 +1,26 @@
+package by.tms.homeapplication
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_hw_two_layout_main.*
+
+class ActivityHwTwoMain : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hw_two_layout_main)
+
+        buttonFlag.setOnClickListener {
+            val intent = Intent(this, ActivityHwTwoOne::class.java)
+            startActivity(intent)
+        }
+
+        buttonAnimated.setOnClickListener {
+            val intent = Intent(this, ActivityHwTwoTwoAnimations::class.java)
+            startActivity(intent)
+        }
+
+    }
+
+
+}
