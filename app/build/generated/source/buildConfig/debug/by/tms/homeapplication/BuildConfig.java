3c843694ca9757f72e243a03f1124d709dc9c6d8
Slava <59572751+GH-Slav@users.noreply.github.com>
2020-03-11 00:59:20 +0300

    13 commit

--- app/build/generated/source/buildConfig/debug/by/tms/homeapplication/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/by/tms/homeapplication/BuildConfig.java
@@ -1,11 +1,11 @@
 /**
  * Automatically generated file. DO NOT MODIFY
  */
-package by.tms.apphome2;
+package by.tms.homeapplication;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String APPLICATION_ID = "by.tms.apphome2";
+  public static final String APPLICATION_ID = "by.tms.homeapplication";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
   public static final int VERSION_CODE = 1;
--- app/src/main/java/by/tms/homeapplication/MainActivity.kt
+++ app/src/main/java/by/tms/homeapplication/MainActivity.kt
@@ -3,6 +3,9 @@ package by.tms.homeapplication
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import by.tms.homeapplication.OneMyFirstApp.ActivityOne
+import by.tms.homeapplication.Three.CoronaVirus
+import by.tms.homeapplication.Two.ActivityTwo
 import kotlinx.android.synthetic.main.activity_main.*
 
 open class MainActivity : AppCompatActivity() {
@@ -12,17 +15,17 @@ open class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         button1.setOnClickListener {
-            val intent = Intent(this, ActivityHwOne::class.java)
+            val intent = Intent(this, ActivityOne::class.java)
             startActivity(intent)
         }
 
         button2.setOnClickListener {
-            val intent = Intent(this, ActivityHwTwoMain::class.java)
+            val intent = Intent(this, ActivityTwo::class.java)
             startActivity(intent)
         }
 
         button3.setOnClickListener {
-            val intent = Intent(this, HwThree::class.java)
+            val intent = Intent(this, CoronaVirus::class.java)
             startActivity(intent)
         }
 
