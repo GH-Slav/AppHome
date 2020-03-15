b7d5f8270bcef7737304c460d5e16c430af4bf06
Slava <59572751+GH-Slav@users.noreply.github.com>
2020-03-15 14:04:10 +0300

    15 commit

---
+++ app/src/main/java/by/tms/homeapplication/FourFlower/activity/FlowerDataTop.kt
@@ -0,0 +1,23 @@
+package by.tms.homeapplication.FourFlower.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import by.tms.homeapplication.R
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.add_flower.*
+import kotlinx.android.synthetic.main.fragment_top_layout.*
+
+class FlowerDataTop : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.fragment_top_layout)
+
+        nameTop.text = intent.getStringExtra(NAME)
+        priceTop.text = intent.getDoubleExtra(PRICE, 0.0).toString()
+        Picasso.get().load(intent.getStringExtra(URL))
+            .placeholder(R.drawable.flower_01).into(imageFragmentTop)
+
+    }
+
+}
---
+++ app/src/main/java/by/tms/homeapplication/Two/Flags.kt
@@ -0,0 +1,13 @@
+package by.tms.homeapplication.Two
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import by.tms.homeapplication.R
+
+class Flags : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.flags)
+    }
+
+}
